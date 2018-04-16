0a90225a5a8f322d286520c7136ca8ba3fd0d5a7
Hython <hython@HythondeMacBook-Pro.local>
2018-04-16 19:56:54 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require('path');
+let app=express();
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.listen(3000,()=>{
+    console.log("The  server is listening on port 3000:");
+});
