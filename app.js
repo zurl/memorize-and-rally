08e3493ad6c812c1a77b548854e35a3ca0f2f9ae
Yuanjian <yuanjian@uchicago.edu>
2021-09-17 00:29:02 +0800

    demo on heruku to show the static website

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 let app=express();
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(3000,()=>{
-    console.log("The  server is listening on port 3000:");
+app.listen(PORT,()=>{
+    console.log(`The  server is listening on port ${PORT}:`);
 });
